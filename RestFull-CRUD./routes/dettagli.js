21d7ea017e03700214d0c712055f3a13a654f8e9
lucatedesco80 <lucatedesco80@gmail.com>
2020-02-11 21:04:18 +0000

    ultimoPunto

---
+++ RestFull-CRUD./routes/dettagli.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+const sql = require('mssql');
+
+const config = {
+    user: 'sblendorio.christian',  //Vostro user name
+    password: 'xxx123#', //Vostra password
+    server: "213.140.22.237",  //Stringa di connessione
+    database: 'sblendorio.christian', //(Nome del DB)
+}
+
+let executeQuery = function (res, query, next) {
+  sql.connect(config, function (err) {
+    if (err) { //Display error page
+      console.log("Error while connecting database :- " + err);
+      res.status(500).json({success: false, message:'Error while connecting database', error:err});
+      return;
+    }
+    var request = new sql.Request(); // create Request object
+    request.query(query, function (err, result) { //Display error page
+      if (err) {
+        console.log("Error while querying database :- " + err);
+        res.status(500).json({success: false, message:'Error while querying database', error:err});
+        sql.close();
+        return;
+      }
+      //uso la mia funzione
+      MandaPug(res, result.recordset);
+      return;
+    });
+    
+  });
+}
+        // qua creo la mia funzione
+        function MandaPug(res, recordset) {
+            let re = recordset[0];// siccome il risultato è dentro le graffe che a sua volta sono dentro le quadre, noi prendiamo solo l'oggetto nella posizione 0, cioè prendiamo solo un risultato
+            res.render('dettagli', {
+                title: `${re.Unit}`,
+                //re: è la variabile di tipo re(che contiene il risultato)
+                re: re
+            });
+            
+        }
+
+        /* GET home page. */
+        router.get('/:unit', function (req, res, next) {
+              let sqlQuery = `select * from dbo.[cr-unit-attributes] where Unit = '${req.params.unit}'`;
+            executeQuery(res, sqlQuery, next);
+    });
+
+    module.exports = router;
+
+ //Cliccando su un’unità si viene reindirizzati alla pagina dettagli in cui è possibile visualizzare tutti i dettagli dell’unità selezionata. 
+// punto2. vale anche per dettagli.pug
--- RestFull-CRUD./routes/index.js
+++ RestFull-CRUD./routes/index.js
@@ -1,9 +1,57 @@
 var express = require('express');
 var router = express.Router();
+const sql = require('mssql');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const config = {
+    user: 'tedesco.luca',  //Vostro user name
+    password: 'xxx123#', //Vostra password
+    server: "213.140.22.237",  //Stringa di connessione
+    database: 'tedesco.luca', //(Nome del DB)
+}
 
-module.exports = router;
+let executeQuery = function (res, query, next) { //query corrisponde ad sqlquery sotto nella get
+  sql.connect(config, function (err) { //fa la connessione con l' ogetto config sopra e gli fa vedere l'errore
+    if (err) { //Display error page
+      console.log("Error while connecting database :- " + err);
+      res.status(500).json({success: false, message:'Error while connecting database', error:err});
+      return;
+    } 
+    // creo una variabile di tipo sql.request 
+    var request = new sql.Request(); 
+    
+    request.query(query, function (err, result) { //Display error page
+      if (err) {
+        console.log("Error while querying database :- " + err);
+        res.status(500).json({success: false, message:'Error while querying database', error:err});
+        sql.close();  // chiude la richiesta ad sql
+        return;
+      }
+
+            // gli manda la funzione
+      MandaPug(res, result.recordset); // resul.recordset è il vettore con dentro il risultato
+      return;
+    });
+    
+  });
+}
+
+    
+        function MandaPug(res, recordset) { //recordset è il risultato
+            res.render('index', {
+                title: 'Tutte le unità:',
+                re: recordset //creo il classico oggetto per prendere le informazioni dal json 
+            });
+            
+        }
+
+         /* ESEGUE LA QUERY SUL SQL E RICHIAMA LA FUNZIONE EXECUTE*/
+        router.get('/', function (req, res, next) {
+            let sqlQuery = "select * from dbo.[cr-unit-attributes]";
+            executeQuery(res, sqlQuery, next);  /*PASSA RES, SQLQUERY (LA QUERY) E L'ERRORE*/
+    });
+
+    module.exports = router;
+
+
+     //Visualizzare il nome e la foto di tutte le unità in una pagina PUG.
+     // punto1 anche index.pug 
